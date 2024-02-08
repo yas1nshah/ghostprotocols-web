469049818d8102c9fcac5b95e484993c5e472035
yas1nshah <yas1nshah@yahoo.com>
2024-02-08 19:07:00 +0500

    added url

--- static-files/urls.js
+++ static-files/urls.js
@@ -1,5 +1,8 @@
+// const  urls= {
+//     APIURL : "http://127.0.0.1:8000/api"
+// }
 const  urls= {
-    APIURL : "http://127.0.0.1:8000/api"
+    APIURL : "https://api.ghostprotocols.pk/api"
 }
 
 
